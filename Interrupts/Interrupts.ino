2ae3142bd15f85ab38fb08e7be49b1b74a861989
David Jung <d.jung460@gmail.com>
2016-03-10 11:24:51 -0800

    Add continuous analog interrupts

--- Interrupts/Interrupts.ino
+++ Interrupts/Interrupts.ino
@@ -2,39 +2,56 @@
  * References:  http://www.instructables.com/id/Arduino-Software-debouncing-in-interrupt-function/
  */
 
-int push_button_pin = 2;
-long debounce_time = 100;
-volatile unsigned long last_micro;
-long processing_time = 15000000;
+const int push_button_pin = 2;
+const long debounce_time = 100;
+volatile unsigned long last_micro_debounce;
+const long processing_time = 15000000;
 int signal_pin = 0;
 
+const int ADC_BUFFER_SIZE = 8;
+volatile uint16_t ADC_counter;
+volatile uint16_t ADC_buffer[ADC_BUFFER_SIZE];
 
 void setup() {
     pinMode(push_button_pin, INPUT);
     attachInterrupt(digitalPinToInterrupt(push_button_pin), debounceButton, CHANGE);
+		enableAnalogInterrupts();
     Serial.begin(9600);
 }
 
 void loop() {
+	Serial.print(ADC_buffer[ADC_counter]);
 }
 
 void debounceButton(){
-  if ((long)(micros()-last_micro) >= debounce_time *1000){
-    interrupt();
+  if ((long)(micros()-last_micro_debounce) >= debounce_time *1000){
+    buttonInterrupt();
     last_micro = micros();
   }
 }
 
-void interrupt() {
-    int signalReadng; 
+void buttonInterrupt() {
     long currentTime = micros();
     while (micros() - currentTime <= processing_time){
-      //Read analog reading and print to serial monitor
-      signalReading = analogRead(signal_pin);
-      Serial.print(signalReading);
-    }
-  
-    int state = digitalRead(push_button_pin);
-    Serial.print("Interrupted in state ");
-    Serial.println(state);
+    } 
 }
+
+void enableAnalogInterrupt() {
+	DIDR0 = 0x3F;
+	ADMUX = 0x43;
+	ADCSRA = 0xAC;
+	ADCSRB = 0x40;
+	bitWrite(ADCSRA, 6, 1);
+	sei();
+}
+
+/*
+ADC takes 13 clock cycles so max frequency is 16 MHz/(16*13) ~= 77 kHz
+*/
+ISR(ADC_vect) {
+	uint16_t = analog_val = ADCL;	// store lower byte from ADC
+	analog_val += ADCH << 8	// store higher byte from ADC
+	ADC_buffer[ADC_counter] = analog_val;
+	ADC_counter = (ADC_counter + 1) % ADC_BUFFER_SIZE;
+}
+
