c22cb4be4b3f9396c22a851002955556b064752e
David Jung <d.jung460@gmail.com>
2016-03-13 13:18:27 -0700

    Analog interrupts working, just need to disply on processing

--- Interrupts/Interrupts.ino
+++ Interrupts/Interrupts.ino
@@ -6,34 +6,30 @@ const int push_button_pin = 2;
 const long debounce_time = 100;
 volatile unsigned long last_micro_debounce;
 const long processing_time = 15000000;
-int signal_pin = 0;
+const int BAUD_RATE = 115200;
 
-const int ADC_BUFFER_SIZE = 8;
+const int ADC_BUFFER_SIZE = 4;
 volatile uint16_t ADC_counter;
 volatile uint16_t ADC_buffer[ADC_BUFFER_SIZE];
 
 void setup() {
     pinMode(push_button_pin, INPUT);
     attachInterrupt(digitalPinToInterrupt(push_button_pin), debounceButton, CHANGE);
-		enableAnalogInterrupts();
-    Serial.begin(9600);
+    enableAnalogInterrupt();
+    Serial.begin(115200);
 }
 
 void loop() {
-	Serial.print(ADC_buffer[ADC_counter]);
+  for(int i = 0; i < ADC_BUFFER_SIZE; i++) {
+    Serial.println(ADC_buffer[i]);
+  }
 }
 
 void debounceButton(){
   if ((long)(micros()-last_micro_debounce) >= debounce_time *1000){
-    buttonInterrupt();
-    last_micro = micros();
+    last_micro_debounce = micros();
+    button_read_state = ~button_read_state;
   }
-}
-
-void buttonInterrupt() {
-    long currentTime = micros();
-    while (micros() - currentTime <= processing_time){
-    } 
 }
 
 void enableAnalogInterrupt() {
@@ -48,10 +44,10 @@ void enableAnalogInterrupt() {
 /*
 ADC takes 13 clock cycles so max frequency is 16 MHz/(16*13) ~= 77 kHz
 */
+//TODO: Use AVR instead of c. Use the instructables tutorial
 ISR(ADC_vect) {
-	uint16_t = analog_val = ADCL;	// store lower byte from ADC
-	analog_val += ADCH << 8	// store higher byte from ADC
-	ADC_buffer[ADC_counter] = analog_val;
-	ADC_counter = (ADC_counter + 1) % ADC_BUFFER_SIZE;
+  uint16_t analog_val = ADCL;	// store lower byte from ADC
+  analog_val |= ADCH << 8;      
+  ADC_buffer[ADC_counter] = analog_val;
+  ADC_counter = (ADC_counter + 1) % ADC_BUFFER_SIZE;
 }
-
